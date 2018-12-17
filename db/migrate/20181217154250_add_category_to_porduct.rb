8ffc2a76faf5288de9978e297ccf5c940372e464
augustin cousin <augustincousin.pro@gmail.com>
2018-12-17 17:20:08 +0100

    show

---
+++ db/migrate/20181217154250_add_category_to_porduct.rb
@@ -0,0 +1,5 @@
+class AddCategoryToPorduct < ActiveRecord::Migration[5.2]
+  def change
+     add_column :products, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_142404) do
+ActiveRecord::Schema.define(version: 2018_12_17_154250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_12_17_142404) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "purchases", force: :cascade do |t|
