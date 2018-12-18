d7233e8220f857d1666c330a9940df81d90b86ad
augustin cousin <augustincousin.pro@gmail.com>
2018-12-18 10:33:21 +0100

    comment create_show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -26,31 +26,31 @@ def create
     end
   end
 
-  def create_show
-    @product = Product.find(params[:product])
-    @cart = Cart.new
-    @cart.user = current_user
-    @cart.product = @product
-    authorize @cart
-    @cart.save
-    @counter = current_user.carts.count
-    respond_to do |format|
-      format.html { redirect_to products_path }
-      format.js
-    end
-  end
+  # def create_show
+  #   @product = Product.find(params[:product])
+  #   @cart = Cart.new
+  #   @cart.user = current_user
+  #   @cart.product = @product
+  #   authorize @cart
+  #   @cart.save
+  #   @counter = current_user.carts.count
+  #   respond_to do |format|
+  #     format.html { redirect_to products_path }
+  #     format.js
+  #   end
+  # end
 
-  def destroy_show
-    @product = Product.find(params[:id])
-    @cart = Cart.find_by(product_id: params[:id])
-    authorize @cart
-    @cart.destroy
-    @counter = current_user.carts.count
-    respond_to do |format|
-      format.html { redirect_to products_path }
-      format.js
-    end
-  end
+  # def destroy_show
+  #   @product = Product.find(params[:id])
+  #   @cart = Cart.find_by(product_id: params[:id])
+  #   authorize @cart
+  #   @cart.destroy
+  #   @counter = current_user.carts.count
+  #   respond_to do |format|
+  #     format.html { redirect_to products_path }
+  #     format.js
+  #   end
+  # end
 
 
 end
