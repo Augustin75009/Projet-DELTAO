119cac6e63d540f037b56cc70769dac38fe5c6e1
Diane Johnston <diane.jroussillon@gmail.com>
2018-12-19 10:34:26 +0100

    finished changing words

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,32 +9,6 @@ class PaymentsController < ApplicationController
   private
 
   def set_purchase
-    @purchase = current_user.purchases.where(state: 'pending').find(params[:order_id])
+    @purchase = current_user.purchases.where(state: 'pending').find(params[:purchase_id])
   end
-
-
-
-  # def create
-  # @purchase = Purchase.find(params[:adoption_id])
-  # customer = Stripe::Customer.create(
-  #     source: params[:stripeToken],
-  #     email:  params[:stripeEmail]
-  #   )
-
-  # charge = Stripe::Charge.create(
-  #     customer:     customer.id,
-  #     amount:       @purchase.product.price_cents,
-  #     description:  "Payment for #{@purchase.product.name}",
-  #     currency:     "EUR"
-  # )
-
-  # @purchase.update(status: 'paid')
-  # # authorize @adoption
-  # redirect_to status_path
-
-  # rescue Stripe::CardError => e
-  #   flash[:alert] = e.message
-  #   redirect_to status_path
-  # end
-
 end
--- db/migrate/20181218145231_create_purchases.rb
+++ db/migrate/20181218145231_create_purchases.rb
@@ -2,7 +2,7 @@ class CreatePurchases < ActiveRecord::Migration[5.2]
   def change
     create_table :purchases do |t|
       t.string :state
-      t.string :teddy_sku
+      t.string :product_sku
       # t.monetize :amount, currency: { present: false }
       t.jsonb :payment
       t.references :user, foreign_key: true
