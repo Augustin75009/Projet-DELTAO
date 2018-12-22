c68b652a0bb95504b855c8520e5faceeb5f26cb4
augustin cousin <augustincousin.pro@gmail.com>
2018-12-22 13:57:28 +0100

    show recapitulatif

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,7 @@ class CartsController < ApplicationController
   end
 
   def show
-    # @cart = Cart.find(@cart_id)
+    @cart = Cart.find(params[:id])
   end
 
 
@@ -37,32 +37,6 @@ class CartsController < ApplicationController
       format.js
     end
   end
-
-  # def create_show
-  #   @product = Product.find(params[:product])
-  #   @cart = Cart.new
-  #   @cart.user = current_user
-  #   @cart.product = @product
-  #   authorize @cart
-  #   @cart.save
-  #   @counter = current_user.carts.count
-  #   respond_to do |format|
-  #     format.html { redirect_to products_path }
-  #     format.js
-  #   end
-  # end
-
-  # def destroy_show
-  #   @product = Product.find(params[:id])
-  #   @cart = Cart.find_by(product_id: params[:id])
-  #   authorize @cart
-  #   @cart.destroy
-  #   @counter = current_user.carts.count
-  #   respond_to do |format|
-  #     format.html { redirect_to products_path }
-  #     format.js
-  #   end
-  # end
 
   private
 
