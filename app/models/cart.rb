c9a16f1a4bb2e35cec279a400ad0408802b00fb1
augustin cousin <augustincousin.pro@gmail.com>
2018-12-22 14:31:28 +0100

    added dependences

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :user
   monetize :price_cents
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   monetize :price_cents
+  has_many :cart_items, dependent: :nullify
 end
