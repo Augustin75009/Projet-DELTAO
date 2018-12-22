832d0002beaad6062e418d5d56db49f702dac99e
augustin cousin <augustincousin.pro@gmail.com>
2018-12-22 15:20:04 +0100

    added quantity to cart_item

---
+++ db/migrate/20181222141916_add_quantity_to_cart_item.rb
@@ -0,0 +1,5 @@
+class AddQuantityToCartItem < ActiveRecord::Migration[5.2]
+  def change
+    add_column :cart_items, :quantity, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_155756) do
+ActiveRecord::Schema.define(version: 2018_12_22_141916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_12_19_155756) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "product_id"
+    t.integer "quantity"
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
     t.index ["product_id"], name: "index_cart_items_on_product_id"
     t.index ["user_id"], name: "index_cart_items_on_user_id"
