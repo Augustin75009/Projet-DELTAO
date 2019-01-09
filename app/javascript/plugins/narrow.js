8a45eca50a665222ff49e34b808c532a211a8b5d
augustin cousin <augustincousin.pro@gmail.com>
2019-01-09 21:14:10 +0100

    ajax panier

--- app/javascript/plugins/narrow.js
+++ app/javascript/plugins/narrow.js
@@ -2,31 +2,8 @@ const updateQuantityUp = () => {
  document.querySelector('.narrow-input').addEventListener('click', (event) => {
   event.preventDefault();
   const value = event.currentTarget.value;
-  // console.log("hello")
+  console.log(value)
   });
 }
 
 export {updateQuantityUp}
-
-// function updateQuantity(data) {
-//   const quantity = document.querySelector('.quantity-item');
-//   // const value = quantity.value;
-//   console.log("hello");
-//   // const amount = document.querySelector('.infos-item');
-//   const value = data;
-//   console.log(value)
-//   // newReviewForm.innerHTML = innerHTML;
-// }
-
-
-// // form.addEventListener("submit", (event) => {
-// //   event.preventDefault();
-// //   // const data = {author: amount.value};
-// //   // updateQuantity(data);
-// //   console.log("hello")
-// // });
-
-// updateQuantity("<%= j render 'cart_items/variable', cart_item: cart_item %>");
-
-// // refresh.addEventListener("click", updateQuantity)
-
