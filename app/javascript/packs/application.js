2b69abca3e2bd543061e6ff130ebb0c064b33777
augustin cousin <augustincousin.pro@gmail.com>
2019-01-23 20:57:59 +0100

    style on show & js on cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import { initSlick } from '../plugins/slick';
 import { updateQuantityUp } from '../plugins/narrow';
 // import {} from '../plugins/show';
 import {selectLesson} from '../plugins/home';
+import {infoAnimation} from '../plugins/card';
 
 initSlick();
 // updateQuantityUp();
 selectLesson();
+infoAnimation();
---
+++ app/javascript/plugins/card.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll('.card-dispatch-lesson');
+// const info = document.querySelector('.lesson-infos');
+
+const infoAnimation = () => {
+ cards.forEach((card) => {
+  const info = card.childNodes[2].childNodes[0].childNodes[5];
+    card.addEventListener('mouseover', (event) => {
+      info.style.animation = "infoUp 0.2s 1 forwards"
+    });
+    card.addEventListener('mouseout', (event) => {
+      info.style.animation = "infoDown 0.2s 1 forwards"
+    });
+  });
+}
+
+export {infoAnimation}
