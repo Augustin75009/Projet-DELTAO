b2f005621ffbeb21bbf6cafeb3b780852b3b7ea8
augustin cousin <augustincousin.pro@gmail.com>
2019-04-06 18:16:15 +0200

    style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,15 @@
 import "bootstrap";
+import "plugins/flatpickr"
 import { initSlick } from '../plugins/slick';
 import { updateQuantityUp } from '../plugins/narrow';
-// import {} from '../plugins/show';
 import {selectLesson} from '../plugins/home';
 import {infoAnimation} from '../plugins/card';
-import {eventSticked} from '../plugins/eventSticked';
 import {phoneNumber} from '../plugins/phoneNumber';
 import {tabFocus} from '../plugins/tab';
 import {activedFilter} from '../plugins/activedFilter';
 import {modifyWebsite} from '../plugins/open-widget';
+
+
 
 initSlick();
 // updateQuantityUp();
@@ -17,11 +18,6 @@ infoAnimation();
 
 if(document.location.href.match('lessons/')) {
   phoneNumber();
-}
-
-
-if(document.location.href.match('lessons')) {
-  eventSticked();
 }
 
 if(document.location.href.match('query')) {
