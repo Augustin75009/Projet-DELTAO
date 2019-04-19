09faad83f9579cade1ae524833bdaf366d9da4b6
augustin cousin <augustincousin.pro@gmail.com>
2019-04-19 08:47:29 +0200

    added link with query in overview

--- app/javascript/plugins/home.js
+++ app/javascript/plugins/home.js
@@ -21,37 +21,32 @@ const selectLesson = () => {
     if (course === "Cours enfants"){
       background.classList.add('background1');
       button.classList.add('upper');
-      // content.appendChild(content.childNodes[1])
       content.childNodes[1].innerHTML =
-      "<h3>Cours enfants</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons'><button class='button-infos overview'>En savoir +</button></a>"
+      "<h3>Cours enfants</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons?query1=true'><button class='button-infos overview'>En savoir +</button></a>"
     }
     if (course === "Cours adultes") {
       background.classList.add('background2');
       button.classList.add('upper');
-      // content.appendChild(content.childNodes[1])
       content.childNodes[1].innerHTML =
-      "<h3>Cours adultes</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons'><button class='button-infos overview'>En savoir +</button></a>"
+      "<h3>Cours adultes</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons?query2=true'><button class='button-infos overview'>En savoir +</button></a>"
     }
     if (course === "Découverte") {
       background.classList.add('background3');
       button.classList.add('upper');
-      // content.appendChild(content.childNodes[1])
       content.childNodes[1].innerHTML =
-      "<h3>Cours découverte</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons'><button class='button-infos overview'>En savoir +</button></a>"
+      "<h3>Cours découverte</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons?query3=Découverte'><button class='button-infos overview'>En savoir +</button></a>"
     }
     if (course === "Atelier libre") {
       background.classList.add('background4');
       button.classList.add('upper');
-      // content.appendChild(content.childNodes[1])
       content.childNodes[1].innerHTML =
-      "<h3>Atleier libre</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons'><button class='button-infos overview'>En savoir +</button></a>"
+      "<h3>Atleier libre</h3><p>Description du cours .... <br>....... <br>.... ......</p><a href='/lessons?query4=Atelier libre'><button class='button-infos overview'>En savoir +</button></a>"
     }
     if (course === "Stage") {
       background.classList.add('background5');
       button.classList.add('upper');
-      // content.appendChild(content.childNodes[1])
       content.childNodes[1].innerHTML =
-      "<h3>Stage</h3><p>Description du cours .... <br>....... <br>... ......</p><a href='/lessons'><button class='button-infos overview'>En savoir +</button></a>"
+      "<h3>Stage</h3><p>Description du cours .... <br>....... <br>... ......</p><a href='/lessons?query5=Stage'><button class='button-infos overview'>En savoir +</button></a>"
     }
     });
   });
