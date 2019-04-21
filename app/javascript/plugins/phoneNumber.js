f5ee0cf2d06cb7833cb136e9037fa50c2e2ef735
augustin cousin <augustincousin.pro@gmail.com>
2019-04-21 18:19:56 +0200

    Revert "test"
    This reverts commit 2f162df31b8bcbeb532ad4fca16073b5bd80949c.

--- app/javascript/plugins/phoneNumber.js
+++ app/javascript/plugins/phoneNumber.js
@@ -8,26 +8,26 @@ const phoneNumber = () => {
       phoneButton.style.color = "rgb(210, 139, 128)"
       phoneButton.style.fontWeight = "bold"
 
-      // const selection = window.getSelection();
-      // const range = document.createRange();
-      // range.selectNodeContents(copyText);
-      // selection.removeAllRanges();
-      // selection.addRange(range);
+      const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(copyText);
+        selection.removeAllRanges();
+        selection.addRange(range);
 
-      // try {
-      //     document.execCommand('copy');
-      //     selection.removeAllRanges();
-      //     document.querySelector(".copied-box").classList.add("showBox");
+        try {
+            document.execCommand('copy');
+            selection.removeAllRanges();
+            document.querySelector(".copied-box").classList.add("showBox");
 
-      //     setTimeout(() => {
-      //       document.querySelector(".copied-box").classList.remove("showBox");
-      //     }, 4000);
-      // } catch (e) {
-      //     copyText.textContent = 'Couldn\'t copy, hit Ctrl+C!';
+            setTimeout(() => {
+              document.querySelector(".copied-box").classList.remove("showBox");
+            }, 4000);
+        } catch (e) {
+            copyText.textContent = 'Couldn\'t copy, hit Ctrl+C!';
 
-      //     setTimeout(() => {
-      //     }, 1200);
-      // }
+            setTimeout(() => {
+            }, 1200);
+        }
     });
 }
 
