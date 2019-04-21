8664296f76ade8aca34402774c427416334d18b0
augustin cousin <augustincousin.pro@gmail.com>
2019-04-21 17:12:13 +0200

    to be remove

--- app/javascript/plugins/phoneNumber.js
+++ app/javascript/plugins/phoneNumber.js
@@ -7,6 +7,27 @@ const phoneNumber = () => {
       phoneButton.style.background = "#DBDBDB"
       phoneButton.style.color = "rgb(210, 139, 128)"
       phoneButton.style.fontWeight = "bold"
+
+      const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(copyText);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        try {
+            document.execCommand('copy');
+            selection.removeAllRanges();
+            document.querySelector(".copied-box").classList.add("showBox");
+
+            setTimeout(() => {
+              document.querySelector(".copied-box").classList.remove("showBox");
+            }, 4000);
+        } catch (e) {
+            copyText.textContent = 'Couldn\'t copy, hit Ctrl+C!';
+
+            setTimeout(() => {
+            }, 1200);
+        }
     });
 }
 
