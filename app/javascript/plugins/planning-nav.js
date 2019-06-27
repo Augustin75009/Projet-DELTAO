cd03c2a7988ab0b33e2b84e0e4e5d889f0642798
augustin cousin <augustincousin.pro@gmail.com>
2019-06-27 12:02:30 +0200

    new planning

--- app/javascript/plugins/planning-nav.js
+++ app/javascript/plugins/planning-nav.js
@@ -10,9 +10,9 @@ const planningNav = () => {
       confirmButtonColor: '#c28d65',
     });
     Toast.fire({
-      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1560761290/PLANNING-rentre%CC%81e-16-1.png',
-      width: 1200,
-      imageHeight: h,
+      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1561629524/Capture_d_e%CC%81cran_2019-06-27_a%CC%80_11.58.01.png',
+      width: 1400,
+      imageHeight: 200,
       animation: false
     });
   })
@@ -22,9 +22,9 @@ const planningNav = () => {
       confirmButtonColor: '#c28d65',
     });
     Toast.fire({
-      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1560761290/PLANNING-rentre%CC%81e-16-1.png',
-      width: 1200,
-      imageHeight: h,
+      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1561629524/Capture_d_e%CC%81cran_2019-06-27_a%CC%80_11.58.01.png',
+      width: 1400,
+      imageHeight: 200,
       animation: false
     });
   })
--- app/javascript/plugins/planning.js
+++ app/javascript/plugins/planning.js
@@ -10,7 +10,7 @@ const planning = () => {
       confirmButtonColor: '#c28d65',
     });
     Toast.fire({
-      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1560761290/PLANNING-rentre%CC%81e-16-1.png',
+      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1561629524/Capture_d_e%CC%81cran_2019-06-27_a%CC%80_11.58.01.png',
       width: 1200,
       imageHeight: h,
       animation: false
@@ -22,7 +22,7 @@ const planning = () => {
       confirmButtonColor: '#c28d65',
     });
     Toast.fire({
-      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1560761290/PLANNING-rentre%CC%81e-16-1.png',
+      imageUrl: 'https://res.cloudinary.com/dw7nokc0j/image/upload/v1561629524/Capture_d_e%CC%81cran_2019-06-27_a%CC%80_11.58.01.png',
       width: 1200,
       imageHeight: h,
       animation: false
