e6c3ac13e9e02d01204727309b9ded0bb208286f
augustin cousin <augustincousin.pro@gmail.com>
2019-08-21 17:37:27 +0200

    domain in production.rd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'http://www.test-augustin.xyz/' }
+  config.action_mailer.default_url_options = { host: 'http://www.delaterrealobjet.fr' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
