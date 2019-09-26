f2b6b1405666d0421afb12dfef86f74574a693a8
augustin cousin <augustincousin.pro@gmail.com>
2019-09-26 21:48:11 +0200

    added mailer sendinblue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,18 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'http://www.delaterrealobjet.fr' }
+  config.action_mailer.default_url_options = { host: 'delaterrealobjet.fr' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp-relay.sendinblue.com',
+    port: 587,
+    user_name: ENV["SENDINBLUE_EMAIL"],
+    password: ENV["SENDINBLUE_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
