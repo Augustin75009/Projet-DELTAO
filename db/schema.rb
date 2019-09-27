7b930dd2f296e01601fdd1f34a3395e5c7db550a
augustin cousin <augustincousin.pro@gmail.com>
2019-09-27 23:24:05 +0200

    default value event priority

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_180542) do
+ActiveRecord::Schema.define(version: 2019_09_27_212241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_180542) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "link"
-    t.integer "priority"
+    t.integer "priority", default: 1
     t.string "alt", default: "Atelier de céramique paris 18"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
