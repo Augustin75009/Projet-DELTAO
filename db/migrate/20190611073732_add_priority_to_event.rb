6e3d327dbb14e75ffc46a42cd9bacf957f0cb662
augustin cousin <augustincousin.pro@gmail.com>
2019-09-27 23:21:18 +0200

    sort event

--- db/migrate/20190611073732_add_priority_to_event.rb
+++ db/migrate/20190611073732_add_priority_to_event.rb
@@ -1,5 +1,5 @@
 class AddPriorityToEvent < ActiveRecord::Migration[5.2]
   def change
-    add_column :events, :priority, :integer, defaut: 0
+    add_column :events, :priority, :integer, default: 0
   end
 end
