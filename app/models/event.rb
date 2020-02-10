5ff55a4cbe6d4ffdb97df719c7daddad7da770a3
augustin cousin <augustincousin.pro@gmail.com>
2020-02-10 11:58:56 +0100

    date not mandatory for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,4 @@
 class Event < ApplicationRecord
   # belongs_to :user, optional: true
   mount_uploader :photo, PhotoUploader
-
-  validates :start_date, presence: true
-  validates :end_date, presence: true
 end
