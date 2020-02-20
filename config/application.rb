8d6f09c01fad89c00fe1ea4b136025b046ceaf80
augustin cousin <augustincousin.pro@gmail.com>
2020-02-20 17:39:11 +0100

    config application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module Delta
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.serve_static_assets = true
+    #config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
