df44ea2e3c87851a4ab676846e27db94203f5895
augustin cousin <augustincousin.pro@gmail.com>
2020-02-21 11:29:24 +0100

    overide slick.scss

--- app/javascript/plugins/slick.js
+++ app/javascript/plugins/slick.js
@@ -3,14 +3,16 @@ import slick from 'slick-carousel'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
+// save : <i class="fas fa-chevron-circle-right slick"></i>
+
 
 
 const initSlick = () => {
  $(document).ready(function(){
       $('.list-carrousel').slick({
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-circle-left slick"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-circle-right slick"></i></button>'
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
       });
     });
 }
