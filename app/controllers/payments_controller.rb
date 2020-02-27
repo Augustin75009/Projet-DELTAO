d41a207b0302e954cf63fcab67d564661c9be64a
augustin cousin <augustincousin.pro@gmail.com>
2020-02-27 14:20:16 +0100

    success_url test environement

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,10 +7,11 @@ class PaymentsController < ApplicationController
     @cart = Cart.find(params[:cart_id])
     @cart_items = CartItem.where(user_id: current_user.id)
     if params[:gift]
-      success_url = "http://localhost:3000/charge?gift=#{params[:gift_id]}"
-      # success_url = "https://www.delaterrealobjet.fr/charge?success=#{params[:gift_id]}"
-      cancel_url = "http://localhost:3000/cart_items"
-      # cancel_url = "https://www.delaterrealobjet.fr/cart_items"
+      # success_url = "http://localhost:3000/charge?gift=#{params[:gift_id]}"
+      # success_url = "https://www.delaterrealobjet.fr/charge?gift=#{params[:gift_id]}"
+      success_url = "https://tessa.herokuapp.com/charge?gift=#{params[:gift_id]}"
+      # cancel_url = "http://localhost:3000/cart_items"
+      cancel_url = "https://www.delaterrealobjet.fr/cart_items"
       @stripe_session = Stripe::Checkout::Session.create(
         customer_email: current_user.email,
         payment_method_types: ['card'],
@@ -32,10 +33,11 @@ class PaymentsController < ApplicationController
         locale: 'fr'
       )
     else
-      success_url = "http://localhost:3000/charge?success=#{params[:purchase_id]}"
+      # success_url = "http://localhost:3000/charge?success=#{params[:purchase_id]}"
       # success_url = "https://www.delaterrealobjet.fr/charge?success=#{params[:purchase_id]}"
-      cancel_url = "http://localhost:3000/cart_items"
-      # cancel_url = "https://www.delaterrealobjet.fr/cart_items"
+      success_url = "https://tessa.herokuapp.com/charge?success=#{params[:purchase_id]}"
+      # cancel_url = "http://localhost:3000/cart_items"
+      cancel_url = "https://www.delaterrealobjet.fr/cart_items"
       @stripe_session = Stripe::Checkout::Session.create(
         customer_email: current_user.email,
         payment_method_types: ['card'],
