9d090fa36fd77a3db0038a3aead05b90dc66e5bd
augustin cousin <augustincousin.pro@gmail.com>
2021-11-08 19:54:17 +0100

    sender prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 465,
-    domain: ENV['DOMAIN_NAME'],
     user_name: ENV["SMTP_USER_NAME"],
     password: ENV["SMTP_PASSWORD"],
     authentication: :plain,
